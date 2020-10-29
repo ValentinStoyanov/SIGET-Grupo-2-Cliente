09ca60ac5bc0aa0e3f441fa123268c5c32158505
Chema98 <chema.28.01.1998@gmail.com>
2020-10-29 13:02:26 +0100

    Rama reunion

--- SIGETGrupo2Cliente/src/app/app.module.ts
+++ SIGETGrupo2Cliente/src/app/app.module.ts
@@ -7,11 +7,13 @@ import { LoginComponent } from "./login/login.component";
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { CrearReunionComponent } from './crear reuniones/crear-reunion/crear-reunion.component';
 
 @NgModule({
   declarations: [
     AppComponent, 
-    LoginComponent
+    LoginComponent,
+    CrearReunionComponent
   ],
   imports: [
     BrowserModule,
