0f3b0ecb299ab598086a9761d9cf649beb2689ad
ISMAEL RABANAL MARTÍN <ismael.rabanal@alu.uclm.es>
2020-11-07 21:14:26 +0100

    Configuracion heroku

--- server.js
+++ server.js
@@ -5,14 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/SIGET-Grupo-2-Cliente'));
+app.use(express.static(__dirname + '/dist/sigetgrupo2-cliente'));
 
-app.get('/*', function(req,res) {
-
-  res.sendFile(path.join(__dirname+'/dist/SIGET-Grupo-2-Cliente/index.html'));
-});
-app.get('*', (req, res) => {
-  res.sendFile(`./front-end/dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
+app.get('*', function(req,res) {
+  res.sendFile(path.join(__dirname+'/dist/sigetgrupo2-cliente/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
