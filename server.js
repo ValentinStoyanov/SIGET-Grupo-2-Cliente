fadb818de65e3d146caf755b1596e7fbeb43b4da
ISMAEL RABANAL MARTÍN <ismael.rabanal@alu.uclm.es>
2020-11-07 21:32:37 +0100

    Configuracion heroku

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static('/sigetgrupo2-cliente'));
 
 app.get('*', function(req,res) {
-  res.sendFile(path.join('/index.html'));
+  res.sendFile('/index.html');
 });
 
 // Start the app by listening on the default Heroku port
