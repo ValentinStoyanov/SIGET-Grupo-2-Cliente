d015abf1e165acd1dd2d3bfac362df9b566e4d8e
ISMAEL RABANAL MARTÍN <ismael.rabanal@alu.uclm.es>
2020-11-07 21:17:34 +0100

    Configuracion heroku

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/sigetgrupo2-cliente'));
+app.use(express.static('./dist/sigetgrupo2-cliente'));
 
 app.get('*', function(req,res) {
-  res.sendFile(path.join(__dirname+'/dist/sigetgrupo2-cliente/index.html'));
+  res.sendFile(path.join('./dist/sigetgrupo2-cliente/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
