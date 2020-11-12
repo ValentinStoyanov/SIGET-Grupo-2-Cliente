91f312b70eec103ca021190342d6bd9a9141f65b
Chema98 <chema.28.01.1998@gmail.com>
2020-11-12 13:04:42 +0100

    crear-reunion

--- src/app/crear-reunion/crear-reunion.component.ts
+++ src/app/crear-reunion/crear-reunion.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ReunionDto } from 'src/app/common/reunion.dto';
+import { Router } from '@angular/router';
+import { ReunionService } from 'src/app/services/reunion.service';
 
 @Component({
   selector: 'app-crear-reunion',
   templateUrl: './crear-reunion.component.html',
   styleUrls: ['./crear-reunion.component.css']
 })
-export class CrearReunionComponent implements OnInit {
+export class CrearReunionComponent {
+  constructor(public router: Router, private reunionServicio: ReunionService) { }
+  temas: string;
+  descripcion: string;
+  fecha: string;
+  hora_fin: string;
+  hora_inicio: string;
+  asistentes: string[];
+  convocante: string;
 
-  constructor() { }
 
-  ngOnInit(): void {
+ crear_reunion() {
+    const reunion: ReunionDto = {
+      temas: this.temas,
+      descripcion: this.descripcion,
+      fecha: this.fecha,
+      hora_fin: this.hora_fin,
+      hora_inicio: this.hora_inicio,
+      asistentes: this.asistentes,
+      convocante: this.convocante
+    }
+    this.reunionServicio.getReunion(reunion)
   }
 
 }
