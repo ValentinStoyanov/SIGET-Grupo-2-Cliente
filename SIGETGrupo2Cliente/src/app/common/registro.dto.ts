c9f803903e988b00034988c4a5bf77233e7a50a6
AfricaSimonSesmero <55829240+AfricaSimonSesmero@users.noreply.github.com>
2020-11-18 12:31:43 +0100

    Create registro.dto.ts

---
+++ SIGETGrupo2Cliente/src/app/common/registro.dto.ts
@@ -0,0 +1,9 @@
+export class RegistroDto {
+    RolId: string;
+    nombre: string;
+    apellidos: string;
+    telefono: number;
+    username: string;
+    email: string;
+    password: string;
+  }
