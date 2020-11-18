ef2147fc571d08ebf964ddee838e85a1323db3d9
AfricaSimonSesmero <55829240+AfricaSimonSesmero@users.noreply.github.com>
2020-11-18 12:29:57 +0100

    Registro

--- SIGETGrupo2Cliente/src/app/registro/registro/registro.component.ts
+++ SIGETGrupo2Cliente/src/app/registro/registro/registro.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { RegistroDto } from 'src/app/common/registro.dto';
+import { UsuarioService } from 'src/app/services/usuario.service';
 
 @Component({
   selector: 'app-registro',
@@ -6,12 +9,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registro.component.css']
 })
 export class RegistroComponent implements OnInit {
-
-  constructor() { }
+    RolId: string;
+    nombre: string;
+    apellidos: string;
+    telefono: number;
+    username: string;
+    email: string;
+    password: string;
+  constructor(public router: Router, private usuarioServicio: UsuarioService) { 
+  }
+  respuesta: RegistroDto;
 
   ngOnInit(): void {
   }
 metodoRegistrar(){
- 
+  const registro: RegistroDto ={
+    RolId: this.RolId,
+    nombre: this.nombre,
+    apellidos: this.apellidos,
+    telefono: this.telefono,
+    username: this.username,
+    email: this.email,
+    password: this.password
+  }
+
+  this.usuarioServicio
+  .getRegistro(registro)
+  .subscribe({
+      next: (resp: RegistroDto)=>{
+        this.respuesta =resp;
+      },
+      error: (err)=>{
+        console.error(err);
+      },
+      complete: () => (console.log("OK")),
+  })
 }
 }
--- SIGETGrupo2Cliente/src/app/services/usuario.service.ts
+++ SIGETGrupo2Cliente/src/app/services/usuario.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { UsuarioDto } from '../common/usuario.dto';
+import { RegistroDto } from '../common/registro.dto';
 
 
 @Injectable({
@@ -12,5 +13,9 @@ export class UsuarioService {
 
   getLogin(usuario: UsuarioDto): any {
     return this.http.post<any>(`http://localhost:8080/usuarios/login?username=${usuario.username}&password=${usuario.password}`, {});
+  }
+
+  getRegistro(usuario: RegistroDto): any {
+    return this.http.post<any>(`http://localhost:8080/usuarios/createUsuario?username=${usuario.username}&password=${usuario.password}&nombre=${usuario.nombre}&apellidos=${usuario.apellidos}&email=${usuario.email}&telefono=${usuario.telefono}`, {});
   }
 }
