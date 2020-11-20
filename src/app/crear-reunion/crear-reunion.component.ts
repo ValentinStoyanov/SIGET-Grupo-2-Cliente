8a5347a4709323179b1ccdbedadc832631a505ae
ISMAEL RABANAL MARTÍN <ismael.rabanal@alu.uclm.es>
2020-11-20 13:09:08 +0100

    Select multiple

--- src/app/crear-reunion/crear-reunion.component.ts
+++ src/app/crear-reunion/crear-reunion.component.ts
@@ -11,17 +11,20 @@ import { UsuarioService } from '../services/usuario.service';
   styleUrls: ['./crear-reunion.component.css']
 })
 export class CrearReunionComponent implements OnInit {
+  constructor(public router: Router, private reunionServicio: ReunionService, private usuarioServicio: UsuarioService) { }
   temas: string;
   descripcion: string;
   horaFin: string;
   horaInicio: string;
-  asistentes: string[];
+  asistentes: string[] = [];
   convocante: string;
- 
-  constructor(public router: Router, private reunionServicio: ReunionService, private usuarioServicio: UsuarioService) { }
   respuesta: ReunionDto;
   nombreUsuario = localStorage.getItem("name");
-  usuariosRegistrados : UsuarioDto[];
+  usuariosRegistrados: UsuarioDto[];
+  usuariosElegidos: UsuarioDto[] = [];
+  indexAdd: number = null;
+  indexDelete: number = null;
+
 
   ngOnInit(): void{
         this.usuarioServicio.getAll().subscribe({
@@ -34,10 +37,10 @@ export class CrearReunionComponent implements OnInit {
       complete: () => (console.log("OK")),
     });
   }
-  
 
- reunion() {
 
+  reunion(): void {
+    console.log(this.asistentes);
     const reunion: ReunionDto = {
       temas: this.temas,
       descripcion: this.descripcion,
@@ -58,10 +61,29 @@ export class CrearReunionComponent implements OnInit {
       },
       complete: () => (console.log("OK")),
     });
-
-
-    
-
   }
 
+  deleteAsistente(): void{
+    if (this.indexDelete != null){
+      this.usuariosRegistrados.push(this.usuariosElegidos[this.indexDelete]);
+      this.asistentes.splice(this.indexDelete, 1);
+      this.usuariosElegidos.splice(this.indexDelete, 1);
+    }
+  }
+
+  addAsistente(): void{
+    if (this.indexAdd != null){
+      this.usuariosElegidos.push(this.usuariosRegistrados[this.indexAdd]);
+      this.asistentes.push(this.usuariosRegistrados[this.indexAdd].username);
+      this.usuariosRegistrados.splice(this.indexAdd, 1);
+
+    }
+  }
+  actualizarIndexAdd(index: number): void {
+    this.indexAdd = index;
+  }
+
+  actualizarIndexDelete(index: number): void {
+    this.indexDelete = index;
+  }
 }
